467b34c745c9e7276ec8c8e75da1dd79583a6a45
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-12-04 12:19:26 +0100

    Register form and token response class.

---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RegisterRequest } from '../model/register-request';
+import { TokenResponse } from '../model/token-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private urlBase = 'http://localhost:8080/auth';
+  
+
+  constructor(private http: HttpClient) {}
+
+  register(req: RegisterRequest) {
+    return this.http.post<TokenResponse>(`${this.urlBase}/register`, req);
+  }
+
+}
--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../core/service/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  registerForm: FormGroup;
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  nameFormControl = new FormControl('', Validators.required);
+  passwordFormControl = new FormControl('', Validators.compose([Validators.required, 
+    Validators.pattern('^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$')]));
+
+
+  constructor(private authService: AuthService, private builder: FormBuilder, private router: Router){
+    
+    this.registerForm = new FormGroup({
+      email: this.emailFormControl,
+      name: this.nameFormControl,
+      password: this.passwordFormControl
+    })
+    
+  }
+
 }
+
