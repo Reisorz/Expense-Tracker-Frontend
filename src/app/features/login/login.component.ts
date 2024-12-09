fdc3b5c149af8a3067265ccc5317753dfc1b3519
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-12-09 08:32:47 +0100

    Login html and css update.

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../core/service/auth.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { AuthRequest } from '../../core/model/auth-request';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule, ToastrModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', Validators.compose([Validators.required, 
+    Validators.pattern('^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8,}$')]));
+
+  request: AuthRequest = new AuthRequest;
+
+
+  constructor(private authService: AuthService, private builder: FormBuilder, private router: Router, private toastr: ToastrService){
+    
+    this.loginForm = new FormGroup({
+      email: this.emailFormControl,
+      password: this.passwordFormControl
+    })
+  }
+
+  loginUser(){
+
+  }
 }
